b27fb153ad0f41cdf89db66500cc77a2b0dfbd11
satya sunarya <satyasunarya@gmail.com>
2019-10-07 15:23:28 +0700

    ok

--- src/example.js
+++ src/example.js
@@ -1,5 +1,4 @@
 import React from "react";
-import About from './pages/about';
 import GetPost from './pages/getpost';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -11,12 +10,7 @@ export default function BasicExample() {
           <li>
             <Link to="/">Home</Link>
           </li>
-          {/* <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li> */}
+   
           <li>
             <Link to="/getpost">Manipulate Element</Link>
           </li>
@@ -25,8 +19,7 @@ export default function BasicExample() {
         <hr />
 
         <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
+  
         <Route path="/getpost" component={GetPost} />
       </div>
     </Router>
@@ -43,36 +36,3 @@ function Home() {
 
 
 
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
-
-function Topic({ match }) {
-  return (
-    <div>
-      <h3>{match.params.topicId}</h3>
-    </div>
-  );
-}
